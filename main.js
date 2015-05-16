607c9f517e2479e95043a4499cf222d5ad78ff00
Chih-Yu Huang <flarehunter@storysensecomputing.com>
2015-05-16 22:04:19 +0800

    Plot the volley field.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var canvas, ctx, image;
 
 // setup a new canvas for drawing wait for device init
 $(document).ready(function() {
@@ -15,46 +16,104 @@ $(document).ready(function() {
     $content.height(content_height);
 
     // Create a canvas as large as the content
-    var canvas = '<canvas id="canvas" width="'+$(window).width()+'" height="'+content_height+'"></canvas>';
-    $content.html(canvas);
-    
+    var canvas_html = '<canvas id="canvas" width="'+$(window).width()+'" height="'+content_height+'"></canvas>';
+    $content.html(canvas_html);
+
     // Set the event listener
     drawTouch();
+
+    // Draw the volley field
+    drawField();
 
 });
 
 // prototype to start drawing on touch using canvas moveTo and lineTo
 var drawTouch = function() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var image = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var start_x, start_y;
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var start_point;
     var header_height = $("#header").outerHeight();
     ctx.lineWidth = 2;
 
     var start = function(e) {
         e.preventDefault();
         console.log('start event', e);
-        start_x = e.changedTouches[0].pageX;
-        start_y = e.changedTouches[0].pageY - header_height;
-        console.log('start:', start_x, start_y);
+        start_point = {
+            x: e.changedTouches[0].pageX,
+            y: e.changedTouches[0].pageY - header_height};
+        console.log('start:', start_point);
     };
     var move = function(e) {
         e.preventDefault();
-        var x = e.changedTouches[0].pageX;
-        var y = e.changedTouches[0].pageY - header_height;
-        ctx.putImageData(image, 0, 0);
-        ctx.beginPath();
-        ctx.moveTo(start_x,start_y);
-        ctx.lineTo(x,y);
-        ctx.stroke();
+        var point = {
+            x : e.changedTouches[0].pageX,
+            y : e.changedTouches[0].pageY - header_height};
+        loadImage();
+        drawLine(start_point, point);
     };
     var end = function(e) {
         e.preventDefault();
-        image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        saveImage();
     }
     document.getElementById("canvas").addEventListener("touchstart", start, false);
     document.getElementById("canvas").addEventListener("touchmove", move, false);
     document.getElementById("canvas").addEventListener("touchend", end, false);
 };
+
+function loadImage() {
+    ctx.putImageData(image, 0, 0);
+}
+function saveImage() {
+    image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function drawLine(p1, p2) {
+    console.log('drawLine', p1, p2);
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+}
+
+function drawField() {
+    // Calculate the scale and the offset of the field
+    var scale = Math.min(canvas.width / 11, canvas.height / 20),
+        offset_x = (canvas.width - 9 * scale) / 2,
+        offset_y = (canvas.height - 18 * scale) / 2;
+    console.log(scale, offset_x, offset_y);
+    function canvas2field(point) {
+        return {
+            x: (point - offset_x) / scale,
+            y: (point - offset_y) / scale};
+    }
+    function field2canvas(point) {
+        return {
+            x: point.x * scale + offset_x,
+            y: point.y * scale + offset_y};
+    }
+
+    // Plot the line
+    function drawFieldLine(p1, p2) {
+        console.log('drawFieldLine', p1, p2);
+        drawLine(field2canvas(p1), field2canvas(p2));
+    }
+
+    // green color inside
+    ctx.fillStyle = '#1B3';
+    ctx.fillRect(offset_x, offset_y, scale * 9, scale * 18);
+
+    // Horizontal line
+    ctx.strokeStyle = '#FFF';
+    var y_arr = [0, 6, 9, 12, 18];
+    for (var i = 0; i < y_arr.length; i++) {
+        drawFieldLine({x:0, y:y_arr[i]}, {x:9, y:y_arr[i]});
+    }
+    // Vertical line
+    var x_arr = [0, 9];
+    for (var i = 0; i < x_arr.length; i++) {
+        drawFieldLine({x:x_arr[i], y:0}, {x:x_arr[i], y:18});
+    }
+    saveImage();
+}
 
